b5a9c7d761ee155fdbbe28d343d59bdd7086bc20
LordsResistanceArmy <LordsResistanceArmy@outlook.com>
2023-10-13 20:35:29 +0800

    progress-60

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,11 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -39,6 +39,17 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let mut t1 = scores.entry(team_1_name).or_insert(
+            Team { goals_scored: (0), goals_conceded: (0) }
+        );
+        t1.goals_scored += team_1_score;
+        t1.goals_conceded += team_2_score;
+
+        let mut t2 = scores.entry(team_2_name).or_insert(
+            Team { goals_scored: (0), goals_conceded: (0) }
+        );
+        t2.goals_scored += team_2_score;
+        t2.goals_conceded += team_1_score;
         
     }
     scores
@@ -73,6 +84,8 @@ mod tests {
     fn validate_team_score_1() {
         let scores = build_scores_table(get_results());
         let team = scores.get("England").unwrap();
+        println!("\n********Lords*********\n{:#?}",scores);
+        println!("{:?}",team);
         assert_eq!(team.goals_scored, 5);
         assert_eq!(team.goals_conceded, 4);
     }
