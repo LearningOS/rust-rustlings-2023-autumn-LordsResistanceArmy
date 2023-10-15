a652609ee3a8e9e5bde4e12294c3fd55aeafd5c0
LordsResistanceArmy <LordsResistanceArmy@outlook.com>
2023-10-15 23:45:50 +0800

    clippy2&3

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -4,17 +4,11 @@
 // hint.
 
 
-
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    
     if let Some(x) = option {
         res += x;
     }
-
-    // for x in option {
-    // }
-    
     println!("{}", res);
 }
